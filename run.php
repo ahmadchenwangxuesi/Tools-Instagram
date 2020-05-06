b313674bc7cd1604403319c6b82a3316050bf754
Rootpaceusa <64696314+ahmadchenwangxuesi@users.noreply.github.com>
2020-05-06 22:13:54 +0700

    Create run.php

---
+++ run.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Name   : Instagram Tools
+ * Tolong jangan di edit, hargai creator jika kalian ingin di hargai orang lain.
+ */
+$banner = '
+  ▄███████████████████████▄
+▄█▀░█░█░█░░░░░░░░░░░░░░░░░░▀█▄
+█░░░█░█░█░░░░░░░░░░░░░█████░░█ |
+█░░░█░█░█░░░░░░░░░░░░░█████░░█ |
+█░░░░░░░░░▄▄▄████▄▄▄░░░░░░░░░█ | AUTHOR  : Pace USA
+███████████▀▀░░░░▀▀███████████ | CREATE  : 06 05 2020
+█░░░░░░░██░░▄████▄░░██░░░░░░░█ | ROOT    : Pace USA
+█░░░░░░░██▄░▀████▀░▄██░░░░░░░█ | VERSION : 1.0
+█░░░░░░░░▀██▄▄░░▄▄██▀░░░░░░░░█ | NAME    : INSTAGRAM TOOLS
+█░░░░░░░░░░▀▀████▀▀░░░░░░░░░░█ |
+█░░░░░░░░░░░░░░░░░░░░░░░░░░░░█ |
+▀█▄░░░░░░░░░WARIFP░░░░░░░░░▄█▀
+  ▀████████████████████████▀
+       INSTAGRAM TOOLS
+   
+[!] Instagram social engineering, and there are several tools from the Instagram feature.       
+  ';
+print $banner;
+?>
